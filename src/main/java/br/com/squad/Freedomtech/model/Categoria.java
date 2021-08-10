fe5d6eb19448e99df1bd9eb798c0aebc4c3b9436
Max_Silva <maxnikollas@gmail.com>
2021-08-10 09:46:17 -0300

    Projeto atualizado com 3 tabelas

--- src/main/java/br/com/squad/Freedomtech/model/Categoria.java
+++ src/main/java/br/com/squad/Freedomtech/model/Categoria.java
@@ -1,14 +1,14 @@
 package br.com.squad.Freedomtech.model;
 
-import java.util.List;
 
-import javax.persistence.CascadeType;
+
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
