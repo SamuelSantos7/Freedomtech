590e25b8fc4027bdb12a40b9960c9563782c05a6
Joelma <joohtrader@gmail.com>
2021-09-28 14:12:44 -0300

    teste unitario,tratamento de erro do postman,carrossel,cadastro e contatos

---
+++ Back-end/src/main/java/br/com/squad/Freedomtech/config/validacao/ErroDeFormularioDto.java
@@ -0,0 +1,38 @@
+package br.com.squad.Freedomtech.config.validacao;
+
+public class ErroDeFormularioDto {
+	
+		private String campo;
+		private String erro;
+		
+		
+		public ErroDeFormularioDto(String campo, String erro) {
+			super();
+			this.campo = campo;
+			this.erro = erro;
+		}
+
+
+		public String getCampo() {
+			return campo;
+		}
+
+
+		public void setCampo(String campo) {
+			this.campo = campo;
+		}
+
+
+		public String getErro() {
+			return erro;
+		}
+
+
+		public void setErro(String erro) {
+			this.erro = erro;
+		}
+		
+		
+	}
+
+
